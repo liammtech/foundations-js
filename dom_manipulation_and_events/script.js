4296da4500f4d4f1d3d83dc2c3f0419a3f38f8e1
liammtech <liammtech@gmail.com>
2023-10-08 14:42:33 +0100

    Add content in line with exercise instructions

--- dom_manipulation_and_events/script.js
+++ dom_manipulation_and_events/script.js
@@ -7,3 +7,27 @@ redParagraph.style.cssText = 'color: red';
 redParagraph.textContent = "Hey I'm red!";
 
 body.appendChild(redParagraph);
+
+// Create blue <h3>
+const blueThirdHeading = document.createElement('h3');
+blueThirdHeading.style.cssText = 'color: blue';
+blueThirdHeading.textContent = "I'm a blue h3!";
+
+body.appendChild(blueThirdHeading);
+
+// Create dive with pink background & black border & child elements 
+// Add everything before adding to body
+
+const blackPinkDiv = document.createElement('div');
+blackPinkDiv.style.backgroundColor = "pink";
+blackPinkDiv.style.border = "2px solid black";
+
+const childHeading = document.createElement("h1");
+childHeading.textContent = "I'm in a div";
+
+const childParagraph = document.createElement("p");
+childParagraph.textContent = "ME TOO!";
+
+blackPinkDiv.appendChild(childHeading);
+blackPinkDiv.appendChild(childParagraph);
+body.appendChild(blackPinkDiv);
